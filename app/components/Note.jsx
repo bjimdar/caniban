2c374dc1713dcf5cac921700e68102d764ae4cbb
Brian Jimdar <brian@jimdar.com>
2016-02-16 15:07:36 -0800

    React enabled and configured with hot-module reloading

---
+++ app/components/Note.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <div>React is up and running.</div>;
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 
 const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
+
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
@@ -12,6 +14,9 @@ const common = {
   // Entry accepts a path or an object of entries.
   // The build chapter contains an example of the latter.
   entry: PATHS.app,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
@@ -19,10 +24,24 @@ const common = {
   module: {
     loaders: [
       {
-        // Test expects a RegExp! Note the slashes!
-        test: /\.css$/,
+        test: /\.css$/,                            // Test expects a RegExp! Note the slashes!
         loaders: ['style', 'css'],
-        // Include accepts either a path or an array of paths.
+        include: PATHS.app                         // Include accepts either a path or an array of paths.
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        query: {
+          // Enable caching for improved performance during development.
+          // It uses default OS directory by default. If you need something
+          // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+          cacheDirectory: true,
+          presets: [
+            'react',
+            'es2015', 
+            'survivejs-kanban'
+          ]
+        },
         include: PATHS.app
       }
     ]
@@ -31,11 +50,8 @@ const common = {
 
 // Default configuration
 if (TARGET === 'start' || !TARGET) {
-
   module.exports = merge(common, {
-    
     devtool: 'eval-source-map',
-
     devServer: {
       contentBase: PATHS.build,
 
@@ -54,9 +70,7 @@ if (TARGET === 'start' || !TARGET) {
       host: process.env.HOST,
       port: process.env.PORT
     },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin()
-    ]
+    plugins: [new webpack.HotModuleReplacementPlugin()]
   });
 }
 

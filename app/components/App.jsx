679ceacc6f154a425811690ff0b465520adff88b
bjimdar <brian@jimdar.com>
2016-02-16 18:44:31 -0800

    Added React component with basic ADD function callback implementation

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,47 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+
 
 export default class App extends React.Component {
-  render() {
-    return <Note />;
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      notes: [
+        { id: uuid.v4(), task: 'Play with WebPack features'},
+        { id: uuid.v4(), task: 'Get React up and running' },
+        { id: uuid.v4(), task: 'Buy a Pumpkin-spiced Latte'} ]
+      
+    };
   }
+  
+  render() {
+    const notes = this.state.notes;
+    
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <ul>{ 
+          notes.map(note => 
+            <li key={note.id}>{note.task}</li> 
+          )
+        }</ul>
+      </div>
+    );
+  }
+  
+  addNote = () => {
+    this.setState({
+      notes: [
+        ...this.state.notes,
+        {
+          id: uuid.v4(), 
+          task: 'New Task placeholder'
+        }
+      ]
+    }, 
+    () => console.log('task added'));
+    
+  };
+  
 }
